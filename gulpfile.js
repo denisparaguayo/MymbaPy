065036a71b47080f1d0f9b3c71b7a6b86c86408c
Denis Ricardo <denis.cantero1987@gmail.com>
2021-03-07 22:19:05 -0300

    agregando dependecias

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const { series, src, dest, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat');
+
+
+
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
+}
+//Función que compila sass
+
+
+
+
+function css() {
+    return src(paths.scss)
+        .pipe(sass())
+        .pipe(dest('./build/css'))
+}
+
+function minificarcss() {
+    return src(paths.scss)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(dest('./build/css'))
+}
+
+function javascript(){
+    return src(paths.js)
+    .pipe(concat('bundle.js'))
+    .pipe(dest('./build/js'))
+}
+
+function imagenes(){
+    return src(paths.imagenes)
+    .pipe(imagemin())
+    .pipe(dest('./build/img'))
+    .pipe(notify({message: 'imagen Minificada'}))
+}
+
+function versionwebp(){
+    return src(paths.imagenes)
+    .pipe(webp())
+    .pipe(dest('./build/img'))
+    .pipe(notify({message: 'Version WebP Listo'}))
+    
+}
+
+function watchArchivos() {
+    watch(paths.scss, css);
+    watch(paths.js, javascript );
+    
+
+}
+
+exports.css = css;
+exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
+exports.watchArchivos = watchArchivos;
+exports.default = series(css, javascript, imagenes, versionwebp, watchArchivos)
