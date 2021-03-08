a0bc3a9b67f682c4d87d53425835a61df8790181
Denis Ricardo <denis.cantero1987@gmail.com>
2021-03-07 23:29:10 -0300

    colores

--- src/js/header.js
+++
@@ -1,6 +0,0 @@
-font-family: 'Kaushan Script', cursive; 
-font-family: 'Pinyon Script', cursive; 
-font-family: 'Petit Formal Script', cursive; 
-font-family: 'Aguafina Script', cursive; 
-font-family: 'Rouge Script', cursive; 
-font-family: 'Roboto', sans-serif;
